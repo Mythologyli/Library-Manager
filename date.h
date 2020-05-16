6a3475674804e4b68165f8d4edf3ca108a484ad4
Mythologyli <lxbpxylps@126.com>
2020-05-16 21:09:07 +0800

    新增date.h文件

---
+++ date.h
@@ -0,0 +1,33 @@
+/*
+文件：date.h
+
+作者：李培生
+
+内容：
+日期结构；
+用于新建日期的函数的声明；
+用于计算间隔天数的函数的声明。
+*/
+
+
+
+
+//日期结构
+struct Date
+{
+    int year;
+    int month;
+    int day;
+};
+
+
+
+
+//新建日期
+struct Date AddDate(int year, int month, int day);
+
+
+
+
+//计算天数间隔
+int DateInterval(struct Date start_date, struct Date end_date);

ac33ef91f123e81da0ec0e940eef379357600a19
Mythologyli <lxbpxylps@126.com>
2020-06-20 20:07:46 +0800

    规范化代码和注释

--- sort.h
+++ sort.h
@@ -11,6 +11,8 @@
 */
 
 
+#ifndef __SORT_H__
+#define __SORT_H__
 
 
 //关于排序为升序或降序的宏定义
@@ -28,3 +30,5 @@ int* SortUserNode(struct UserNode* Node, int node_name, int sort_mode);
 
 //对借阅排序
 int* SortBorrowNode(struct BorrowNode* Node, int node_name, int sort_mode);
+
+#endif
